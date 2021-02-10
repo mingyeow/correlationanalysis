9386506dc9bc17572612c3c6928b0a41775e3d5c
meow <mingyeow@gmail.com>
2021-02-11 01:04:08 +0800

    change code

--- analysis.py
+++ analysis.py
@@ -1,55 +1,74 @@
+#write max supported amplification
+# plot out max deviation
+
 import json
 import requests
+import time
 
 from numpy import mean
 from numpy import std
 from matplotlib import pyplot
 
-def find_ratio(r1, r2):
-    return float(r1["rate"])/float(r2["rate"])
+def get_filename(symbol):
+    return f'{symbol}.json'
 
-# def download_data(symbol):
-#     symbol = "XRP"
-#     curl -X GET "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=usd&from=1592875879&to=1612875879" -H "accept: application/json"
-#     u = f"https://api.nomics.com/v1/exchange-rates/history?key=4f218abd568f894f4c75035587645792&currency={symbol}&start=2020-01-01T00%3A00%3A00Z&end=2021-02-01T00%3A00%3A00Z"
-#     r = requests.get(u)
+def find_ratio(r1, r2):
+    assert (r1[0] == r2[0])
+    return float(r1[1])/float(r2[1])
+
+def find_devitation(r, min_r):
+    return r/min_r
+
+def download_data(symbol):
+    now = int(time.time())
+    year_ago = now - (365*24*60*60)
+
+    u = f"https://api.coingecko.com/api/v3/coins/{symbol}/market_chart/range?vs_currency=usd&from={year_ago}&to={now}"
+    r = requests.get(u)
+    raw_data = r.json()
+    p=raw_data["prices"]
+    return p
+
+name1 = "kyber-network"
+data1 = download_data(name1)
+
+name2 = "usd-coin"
+data2 = download_data(name2)
+
+ratios = list(map(find_ratio, data1, data2))
+
+max = max(ratios)
+min = min(ratios)
+max_deviation = max/min
+
+pyplot.subplot(1, 2, 1)
+pyplot.plot(ratios)
+pyplot.xlabel("time")
+pyplot.ylabel(name1 + "/" + name2 + " ratio")
+pyplot.text(0.3, 0.9, "max deviation:" + str(max_deviation), transform=pyplot.gcf().transFigure)
+
+pyplot.subplot(1, 2, 2)
+deviations = [0, 1, 2, 3, 4]
+pyplot.xlabel("proportion to min")
+
+pyplot.plot(deviations)
+
+pyplot.show()
+
+# with open(get_filename(symbol), 'w') as outfile:
+#     json.dump(p, outfile)
 # check if files exists. if not, download
 # try:
 #     f1 = open(file1)
 # except:
 #     download_data(name1)
+# def check_or_download(name):
+#     return True
+# check_or_download(name2)
+# file2 = f"data/{name2}.json"
+# with open(file1) as json_file:
+#     data1 = json.load(json_file)
 
-def check_or_download(name):
-    return True
-
-name1 = "btc"
-check_or_download(name1)
-file1 = f"data/{name1}.json"
-
-name2 = "eth"
-check_or_download(name2)
-file2 = f"data/{name2}.json"
-
-with open(file1) as json_file:
-    data1 = json.load(json_file)
-
-with open(file2) as json_file:
-    name2 = "eth"
-    data2 = json.load(json_file)
-
-
-result = map(find_ratio, data1, data2)
-
-ratios = list(result)
-
-pyplot.plot(ratios)
-
-max = max(ratios)
-min = min(ratios)
-ratio_deviation = max/min
-
-pyplot.xlabel("time")
-pyplot.ylabel(name1 + "/" + name2 + " ratio")
-pyplot.text(0.3, 0.9, "max deviation:" + str(max/min), transform=pyplot.gcf().transFigure)
-
-pyplot.show()
+# with open(file2) as json_file:
+#     name2 = "eth"
+#     data2 = json.load(json_file)

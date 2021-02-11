b4eb95810d6a4d7f6c8f3328d228f3c80e539190
meow <mingyeow@gmail.com>
2021-02-11 11:17:50 +0800

    found amp formula

--- analysis.py
+++ analysis.py
@@ -20,7 +20,8 @@ def find_devitation(r, min_r):
     return r/min_r
 
 def find_recommended_amp(d):
-    return 1
+    x = d/(1-d)
+    return (x+x**2)**0.5 -x
 
 def download_data(symbol):
     now = int(time.time())
@@ -43,11 +44,12 @@ def find_coin_id(symbol):
 # get id and data for first coin
 name1 = input("First Symbol? ")
 id1 = find_coin_id(name1)
-data1 = download_data(id1)
 
-# get id and data for second coin
 name2 = input("Second Symbol? ")
 id2 = find_coin_id(name2)
+
+#  get data
+data1 = download_data(id1)
 data2 = download_data(id2)
 
 # create the ratios
@@ -56,17 +58,21 @@ ratios = list(map(find_ratio, data1, data2))
 # get min/max and max_deviations
 max = max(ratios)
 min = min(ratios)
+
 max_deviation = max/min
 recommended_amp = find_recommended_amp(max_deviation)
+standard_deviation = std(ratios)
 
 #get the ratios
 ratio_deriatives = list(map(lambda r: r/min, ratios))
 
-pyplot.text(0.15, 0.95, f"max deviation: {str(max_deviation)}" , transform=pyplot.gcf().transFigure)
-pyplot.text(0.15, 0.9, f"recommended amp: {str(recommended_amp)}" , transform=pyplot.gcf().transFigure)
+pyplot.text(0.12, 0.95, f"pair: {name1} / {name2}" , transform=pyplot.gcf().transFigure)
+pyplot.text(0.12, 0.9, f"max ratio/min ratio: {'{:.4f}'.format(max_deviation)}" , transform=pyplot.gcf().transFigure)
+pyplot.text(0.5, 0.95, f"max amp to cater: {'{:.4f}'.format(recommended_amp)}" , transform=pyplot.gcf().transFigure)
+pyplot.text(0.5, 0.9, f"standard deviation: {'{:.4f}'.format(std(ratios))}" , transform=pyplot.gcf().transFigure)
 
 # plot the ratios
 pyplot.plot(ratios)
-pyplot.xlabel("time")
+pyplot.xlabel("last year")
 pyplot.ylabel(name1 + "/" + name2 + " ratio")
 pyplot.show()
